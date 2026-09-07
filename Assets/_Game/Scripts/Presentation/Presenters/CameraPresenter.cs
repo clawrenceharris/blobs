f21b3daa5f61b1cb4410a4da1a1a333d5ecf977b
Caleb Harris <clawrenceharris@gmail.com>
2026-09-07 13:41:35 -0400

    Fix camera framing to use board dimensions and aspect ratio

--- Assets/_Game/Scripts/Presentation/Presenters/CameraPresenter.cs
+++ Assets/_Game/Scripts/Presentation/Presenters/CameraPresenter.cs
@@ -3,45 +3,38 @@ using UnityEngine;
 using Blobs.Core;
 namespace Blobs.Presentation
 {
-    
+
     /// <summary>
     /// Positions and sizes an orthographic camera so the current board fits in view.
     /// </summary>
     [RequireComponent(typeof(Camera))]
     public class CameraPresenter : MonoBehaviour
     {
-        [SerializeField] private float aspectRatio;
         [SerializeField] private float padding;
         private Camera cam;
         void Awake()
         {
             cam = GetComponent<Camera>();
         }
-        
 
-    
-    
+
+
+
 
 
         /// <summary>
         /// Centers the camera on the board and adjusts orthographic size using board dimensions.
         /// </summary>
-        public void FitCameraToBoard(BoardState boardState, float cellSize)
+        public void FitCameraToBoard(int width, int height, float cellSize)
         {
-        
-            float x = (boardState.Width - 1) * cellSize;
-            float y = (boardState.Height - 1) * cellSize;
-            Vector3 tempPosition = new(x / 2, y / 2, -1);
 
-            transform.position = tempPosition;
-            if (boardState.Width >= boardState.Height)
-            {
-                cam.orthographicSize = (x / 2 + padding) / aspectRatio;
-            }
-            else
-            {
-                cam.orthographicSize = y / 2 + padding;
-            }
+            float boardWidth = (width - 1) * cellSize;
+            float boardHeight = (height - 1) * cellSize;
+
+            float requiredForHeight = boardHeight * cellSize + padding;
+            float requiredForWidth = (boardWidth * cellSize + padding) / cam.aspect;
+
+            cam.orthographicSize = Mathf.Max(requiredForHeight, requiredForWidth);
 
         }
 
